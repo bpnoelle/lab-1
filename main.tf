2dc9788b001051e2a71f078700025c4ddb3ee5d7
bpnoelle <n.guzniczak@pocztowy.pl>
2025-12-02 14:47:36 +0000

    More complitate validation

--- main.tf
+++ main.tf
@@ -2,6 +2,21 @@ variable "server-name" {
   type        = string
   description = "Name of a server to provision"
 }
+
+locals {
+  minNumberOfServer = 1
+  maxNumerOfServer = 10
+}
+
+variable "number-of-servers" {
+type = number
+description = "Required number od servers"
+  validation {
+  condition = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumerOfServer
+  error_message = "Not supported number od server, it hsould be from the range [${local.minNumberOfServer}, ${local.maxNumerOfServer}]"
+    }
+}
+
 output "out" {
  value = var.server-name 
 }
