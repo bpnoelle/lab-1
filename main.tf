e60137d371a3f0413d79887970f32221cb9b6e95
bpnoelle <n.guzniczak@pocztowy.pl>
2025-12-05 09:01:28 +0000

    kal.2_cd

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 variable "a" {
   type = number
 }
@@ -7,12 +6,12 @@ variable "b"{
   type = number
 }
 
+
 variable "numbers" {
-  type = list(number)
+  type = list(var.a, var.b)
   description = "lista 2 liczb"
   default = [ 4, 2 ]
 }
-
 
 #Operatory + ,-, / ,*
 variable "operand" {
@@ -35,53 +34,11 @@ locals {
     "*" = local.mnozenie
     "/" = local.dzielenie
   }
-
+  wynik_k = contains(keys(local.wynik), var.operand)
 }
-
 
 output "result" {
   value =  var.numbers
 
 }
 
-
-
-
-
-
-/*
-variable "server-name" {
-  type        = string
-  description = "Name of a server to provision"
-}
-
-locals {
-  minNumberOfServer = 1
-  maxNumerOfServer = 10
-}
-
-variable "number-of-servers" {
-type = number
-description = "Required number od servers"
-default = 2
-
-  validation {
-  condition = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumerOfServer
-  error_message = "Not supported number od server, it hsould be from the range [${local.minNumberOfServer}, ${local.maxNumerOfServer}]"
-    }
-}
-
-variable "number-of-disks" {
-  type = number
-}
-
-variable "list-of-names" {
-  type = list(string)
-}
-
- 
-output "list-of-names" {
-    value = "${join(", ",var.list-of-names)}"
-    }
-
-*/
