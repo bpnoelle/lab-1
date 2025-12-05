73c97ecf87ea8cff1f72dcdb988a8b4ac5e61045
bpnoelle <n.guzniczak@pocztowy.pl>
2025-12-05 09:15:22 +0000

    kal.3_niedokonczone

--- main.tf
+++ main.tf
@@ -1,40 +1,57 @@
-variable "a" {
+/*variable "a" {
   type = number
+  description = "pierwsza liczba"
 }
 
 variable "b"{
   type = number
+  description = "druga liczba"
 }
-
+*/
 
 variable "numbers" {
-  type = list(var.a, var.b)
-  description = "lista 2 liczb"
-  default = [ 4, 2 ]
+  type = list(number)
+  description = "lista 2 liczb np [15,3]"
+  default = [ 15, 3 ]
 }
 
+variable "operator" {
+  description = "operacja do wykonania przez kalkulator +, - ,* ,/"
+  type = string
+  
+}
+
+locals {
+  numbers = [var.numbers]
+}
+
+/*
 #Operatory + ,-, / ,*
 variable "operand" {
   type = string
   default = "+"
   description = "operacja do wykonania przez kalkulator"
 }
+*/
 
 locals {
   liczby = var.numbers
 
-  dodawanie = var.a + var.b
-  odejmowanie = var.b - var.b
-  mnozenie  = var.a * var.b
-  dzielenie = var.a / var.b
+  a = length(local.liczby)
+  b =length(local.liczby)
+
+  dodawanie = local.a + local.b
+  //odejmowanie = var.b - var.b
+  //mnozenie  = var.a * var.b
+  //dzielenie = var.a / var.b
 
   wynik = { 
     "+" = local.dodawanie
-    "-" = local.odejmowanie
-    "*" = local.mnozenie
-    "/" = local.dzielenie
+   // "-" = local.odejmowanie
+   // "*" = local.mnozenie
+   // "/" = local.dzielenie
   }
-  wynik_k = contains(keys(local.wynik), var.operand)
+  wynik_k = local.dodawanie 
 }
 
 output "result" {
