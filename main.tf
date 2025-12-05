86e971cc92e9e6783724c52da0d59b52a2b86732
bpnoelle <n.guzniczak@pocztowy.pl>
2025-12-05 08:50:12 +0000

    Kalkulator_ver1_do_poprawy

--- main.tf
+++ main.tf
@@ -1,3 +1,55 @@
+
+variable "a" {
+  type = number
+}
+
+variable "b"{
+  type = number
+}
+
+variable "numbers" {
+  type = list(number)
+  description = "lista 2 liczb"
+  default = [ 4, 2 ]
+}
+
+
+#Operatory + ,-, / ,*
+variable "operand" {
+  type = string
+  default = "+"
+  description = "operacja do wykonania przez kalkulator"
+}
+
+locals {
+  liczby = var.numbers
+
+  dodawanie = var.a + var.b
+  odejmowanie = var.b - var.b
+  mnozenie  = var.a * var.b
+  dzielenie = var.a / var.b
+
+  wynik = { 
+    "+" = local.dodawanie
+    "-" = local.odejmowanie
+    "*" = local.mnozenie
+    "/" = local.dzielenie
+  }
+
+}
+
+
+output "result" {
+  value =  var.numbers
+
+}
+
+
+
+
+
+
+/*
 variable "server-name" {
   type        = string
   description = "Name of a server to provision"
@@ -11,12 +63,25 @@ locals {
 variable "number-of-servers" {
 type = number
 description = "Required number od servers"
+default = 2
+
   validation {
   condition = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumerOfServer
   error_message = "Not supported number od server, it hsould be from the range [${local.minNumberOfServer}, ${local.maxNumerOfServer}]"
     }
 }
 
-output "out" {
- value = var.server-name 
+variable "number-of-disks" {
+  type = number
 }
+
+variable "list-of-names" {
+  type = list(string)
+}
+
+ 
+output "list-of-names" {
+    value = "${join(", ",var.list-of-names)}"
+    }
+
+*/
